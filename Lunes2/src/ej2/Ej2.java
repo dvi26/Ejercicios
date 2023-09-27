830f07bb2f5f7de82ed9ed29ed786acf5530fd6f
dcaro <dcaro@106-08.info2>
2023-09-27 10:23:39 +0200

    ejs

--- Lunes2/src/ej2/Ej2.java
+++ Lunes2/src/ej2/Ej2.java
@@ -8,12 +8,19 @@ public class Ej2 {
 		int entero;
 		Scanner sc = new Scanner(System.in);
 		// Abro el scanner
-		System.out.println("Introduzca un número decimal: ");
+		System.out.println("Introduzca un número entero: ");
 		// System out para preguntar por el número
 		entero = sc.nextInt();
-		double solución = 2;
-		double xd =(solución %= 7);
-		System.out.println("Introduzca un número decimal: " + xd);
+		double resto =(entero %= 7);
+		double multiplo = 0;
+		double b;
+		double suma2 = entero + b;
+		final double resto 2 = (suma2 %= 7);
+		double a = (resto == multiplo?entero:resto);
+		
+		
+		
+		System.out.println("Hay que sumarle: " + a);
 		
 
 	}

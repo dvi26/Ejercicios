d5bfe112d4d8ec1ca9790dc760901387909c0b4d
dcaro <dcaro@106-08.info2>
2023-09-29 09:15:49 +0200

    kk

--- Lunes2/src/ej10/Ej10.java
+++ Lunes2/src/ej10/Ej10.java
@@ -12,10 +12,16 @@ public class Ej10 {
 		Scanner sc = new Scanner(System.in);
 		// Abro el scanner
 		System.out.println("Introduzca la longitud en metros: ");
-		// System out para pregunttar por el valor de n1
+		// System out para pregunttar por el valor de la longitud
 		longitudm = sc.nextDouble();
+		// longitudm = scanner para introducir la longitud con el teclado
 		longitudcm = longitudm * 100;
+		// convertir de m a cm
 		longitudcmE = (int)longitudcm;
+		// convertir de double a int para sacar la parte entera
+		System.out.println("La longitud en centimetros es de: " + longitudcmE + "cm");
+		// system out + longitudcmE para decir cuanto es la longitud en cm
+		sc.close();
 		
 
 	}
